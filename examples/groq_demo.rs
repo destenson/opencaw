981eb394e7e945a05a0c81d4a435a5b71d98127b
Dennis E <git@destenson.digital>
2026-05-01 21:54:18 -0700

    Show crate/file/line in trace output

--- examples/groq_demo.rs
+++ examples/groq_demo.rs
@@ -40,7 +40,9 @@ fn ingest_file(
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_target(false)
+        .with_target(true)
+        .with_file(true)
+        .with_line_number(true)
         .init();
 
     let runtime = Arc::new(Runtime::new()?);
